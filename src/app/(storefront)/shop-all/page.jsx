dbb4671acd5af69a337480b8d3c29ab31b858b94
Areeba <areebasajid531@gmail.com>
2026-05-07 16:11:17 +0500

    add suspense to avoid build error

--- src/app/(storefront)/shop-all/page.jsx
+++ src/app/(storefront)/shop-all/page.jsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ShopAllView from "@/components/shop-all/ShopAllView";
 
 export default function ShopAllPage() {
-  return <ShopAllView />;
+  return (
+    <Suspense fallback={null}>
+      <ShopAllView />
+    </Suspense>
+  );
 }
